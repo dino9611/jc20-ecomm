7edff49a2cfc519eb554e939d8f87aeeb28fb394
dino9611 <aldinorahman36@gmail.com>
2022-03-04 13:52:27 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BsSearch, BsBag } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container  md:px-40 px-10 py-4 flex bg-teal-300">
+        <div className="text-3xl ">Logo</div>
+        <div className="flex ml-auto mr-16 justify-between w-3/6 bg-pink-400">
+          <div className="text-lg tracking-wider">Watches</div>
+          <div className="text-lg tracking-wider">Eyewear</div>
+          <div className="text-lg tracking-wider">Accesories</div>
+          <div className="text-lg tracking-wider">News</div>
+        </div>
+        <div className=" flex justify-between w-1/5 bg-slate-500">
+          <div>
+            <BsSearch />
+          </div>
+          <div className="flex">
+            <AiOutlineUser />
+            Login
+          </div>
+          <div>
+            <BsBag />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
