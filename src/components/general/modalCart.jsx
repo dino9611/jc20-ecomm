182d56ecb2c4a2bbf2bc467cb3fd4e2176f3d4ba
dino9611 <aldinorahman36@gmail.com>
2022-03-11 15:59:31 +0700

    day14 sesi2

--- src/components/general/modalCart.jsx
+++ src/components/general/modalCart.jsx
@@ -5,8 +5,29 @@ import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import { useReducer } from "react";
 import useUser from "../../hooks/useUser";
-
+import { converToRupiah } from "./../../helpers";
+import { FiTrash2 } from "react-icons/fi";
 const INITIAL_STATE = [
+  {
+    id: 1,
+    name: "Singo Maple",
+    image: "https://i.imgur.com/G6x1bGq_d.webp?maxwidth=760&fidelity=grand",
+    stock: 10,
+    price: 1280000,
+    series: "Maple",
+    serieId: 1,
+    qty: 2,
+  },
+  {
+    id: 2,
+    name: "Singo Ebony",
+    image: "https://i.imgur.com/Pr9R9dw_d.webp?maxwidth=760&fidelity=grand",
+    stock: 10,
+    price: 960000,
+    series: "Ebony",
+    serieId: 2,
+    qty: 1,
+  },
   {
     id: 1,
     name: "Singo Maple",
@@ -53,6 +74,19 @@ const reducer = (state, action) => {
 const ModalCart = ({ open, setopen }) => {
   const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
   const { isLogin } = useUser();
+
+  const renderTotal = () => {
+    // let total = 0;
+    // state.forEach((element) => {
+    //   total += element.price * element.qty;
+    // });
+
+    // return total;
+    return state.reduce((prev, val) => {
+      return prev + val.qty * val.price;
+    }, 0);
+  };
+
   const renderCart = () => {
     if (!isLogin) {
       //   kalau belum login ini yang dirender
@@ -60,24 +94,58 @@ const ModalCart = ({ open, setopen }) => {
     }
     return state.map((val, index) => {
       return (
-        <div key={val.id}>
-          <div>
-            {val.name} {val.qty}
+        <div key={val.id} className="text-matoa-text flex h-[25vh] mb-3 ">
+          <div className="w-2/12 px-5 bg-matoa-primary mr-3  ">
+            <div className=" w-[100%] h-[100%]">
+              <img
+                style={{ width: "100%", height: "100%" }}
+                src={val.image}
+                alt={val.name}
+                className="object-cover"
+              />
+            </div>
           </div>
-          <div>
-            <button
-              onClick={() => dispatch({ type: "Tbhqty", index: index })}
-              className="w-1/2 bg-slate-400"
-            >
-              +
-            </button>
-            <button
-              disabled={state[index].qty < 2}
-              className="w-1/2 bg-pink-400 disabled:bg-orange-500"
-              onClick={() => dispatch({ type: "krgqty", index: index })}
-            >
-              -
-            </button>
+          <div className="w-5/12 px-5">
+            <div className="text-xl mb-4">{val.name}</div>
+            <div className="text-lg font-bold mb-2">
+              {converToRupiah(val.price)}
+            </div>
+            <div className="font-thin mb-2">Custom Engrave</div>
+            <div className="font-thin ">"Happy Birthday"</div>
+          </div>
+          <div className="w-5/12 flex flex-col items-end">
+            <div>Select Packaging</div>
+            <select className="text-sm mt-2 bg-white border-[1px] px-2 focus:border-slate-500  border-slate-300">
+              <option value="">Wooden Packaging (Rp.50.000)</option>
+            </select>
+            <div className="mt-5 flex">
+              <div>
+                <button
+                  disabled={state[index].qty < 2}
+                  className="px-2 mr-3 disabled:text-inherit text-white border-[1px] bg-matoa-text-primary    disabled:bg-transparent  disabled:border-slate-300"
+                  onClick={() => dispatch({ type: "krgqty", index: index })}
+                >
+                  -
+                </button>
+              </div>
+              <div className="py-1">{val.qty}</div>
+              <div>
+                <button
+                  onClick={() => dispatch({ type: "Tbhqty", index: index })}
+                  className="px-2 ml-3 text-white disabled:text-inherit   border-[1px]  bg-matoa-text-primary"
+                >
+                  +
+                </button>
+              </div>
+              <div className="py-1 w-[150px]  text-lg ml-3 mr-3 font-semibold">
+                {converToRupiah(val.price * val.qty)}
+              </div>
+              <div className="py-1 text-lg  font-semibold">
+                <button className="p-1 bg-white ml-3 text-sm  border-[1px]  border-matoa-text-primary">
+                  <FiTrash2 className="text-matoa-text-primary" />
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       );
@@ -85,11 +153,24 @@ const ModalCart = ({ open, setopen }) => {
   };
 
   return (
-    <Dialog open={open} onClose={() => setopen(false)}>
+    <Dialog
+      open={open}
+      // sx={{ maxWidth: "100%" }}
+      maxWidth="false"
+      onClose={() => setopen(false)}
+    >
       {/* <DialogTitle>Modal Cart</DialogTitle */}
-      <DialogContent>{renderCart()}</DialogContent>
+      <DialogContent sx={{ minWidth: "1000px" }}>{renderCart()}</DialogContent>
       <DialogActions>
-        {/* <Button onClick={() => setopen(false)}>Close</Button> */}
+        <div className="my-3 w-full ">
+          <Button
+            onClick={() => setopen(false)}
+            variant="contained"
+            sx={{ minWidth: "100%" }}
+          >
+            Checkout {converToRupiah(renderTotal())}
+          </Button>
+        </div>
       </DialogActions>
     </Dialog>
   );
